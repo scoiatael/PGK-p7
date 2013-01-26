bc4f9c8eab664f8e2de587c5dfa4a8239f2bc0e0
scoiatael <czapl.luk@gmail.com>
2013-01-26 16:32:27 +0100

    Added image lib for texture

---
+++ gli/CMakeFiles/gli.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xubuntu/luki/Documents/PGK/p7/gli/core/dummy.cpp" "/home/xubuntu/luki/Documents/PGK/p7/gli/CMakeFiles/gli.dir/core/dummy.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "external/AntTweakBar-1.15/include"
+  "external/glfw-2.7.6/include"
+  "external/glm-0.9.4.0"
+  "external/glew-1.9.0/include"
+  "."
+  "gli/.."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ gli/CMakeFiles/gli.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gli.dir/core/dummy.cpp.o"
+  "gli.pdb"
+  "gli"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
